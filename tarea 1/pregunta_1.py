2ac76c4b1d37b8db763265e362c2896a765dd954
The2Troller <renatosau@gmail.com>
2026-09-07 21:56:54 -0300

    funciones para mostrar y obtener

--- tarea 1/pregunta_1.py
+++ tarea 1/pregunta_1.py
@@ -1,13 +1,21 @@
 import numpy as np
+import skimage
+import cv2
+from tkinter import filedialog
+# uso de plt debido a incompatibilidades con ubuntu 
+import matplotlib.pyplot as plt   
 
 class ColorSaturation():
     def __init__(self, img, pts, mode):
-        self.img = img # (...)
+        self.img = img # string
         self.hue = pts [0] # tupla x
         self.mul = pts [1] # tupla y
-        self.mode = mode # False = HS(...) True = CIE L*c*h
+        self.mode = mode # Bool; False = HS(...) True = CIE L*c*h
 
-
+    def show(self) -> None:
+        plt.imshow(img)
+        plt.axis("off")
+        plt.show()
 
 
 
@@ -15,4 +23,13 @@ class ColorSaturation():
 
 
 if __name__ == "__main__":
-    colorsaturation = ColorSaturation()
+    my_path = filedialog.askopenfilename()
+    img = cv2.imread(my_path)
+    pts_x = int(input("x: "))
+    pts_y = int(input("y: "))
+    if input("Switch to CIE L*c*h? (Y) Default = HS... : ") == "Y":
+        mode = True
+    else:
+        mode = False
+    colorsaturation = ColorSaturation(img, (pts_x, pts_y), mode)
+    colorsaturation.show()
