09bd32b8043e15da7e9545243bde0f4f98d76c0a
The2Troller <renatosau@gmail.com>
2026-09-16 00:52:47 -0300

    recordatorio de leer la pauta BIEN, y p3 realizada

--- tarea 1/pregunta_3.py
+++ tarea 1/pregunta_3.py
@@ -3,7 +3,6 @@ import cv2
 from tkinter import filedialog
 import matplotlib.pyplot as plt # uso de plt debido a incompatibilidades con ubuntu 
 
-
 class Scaling():
     def __init__(self, img, scale : float = 1.0, mode : bool = False):
         self.img = img
@@ -16,72 +15,40 @@ class Scaling():
         
     def process(self):
         if self.mode:
-            if self.scale >= 1.0:
-                pass
-            else:
-                self.final_img = self.downscaling(self.img)
+            self.final_img = self.scale_img(self.img)
+            self.final_img = self.scale_img(self.img)
         else:
-            if self.scale >= 1.0:
-                r = self.upscaling(self.img[:, :, 2])
-                g = self.upscaling(self.img[:, :, 1])
-                b = self.upscaling(self.img[:, :, 0])
-            else:
-                r = self.downscaling(self.img[:, :, 2])
-                g = self.downscaling(self.img[:, :, 1])
-                b = self.downscaling(self.img[:, :, 0])
-                self.final_img = np.stack((r, g, b), axis = -1).astype(np.uint8)
+            r = self.scale_img(self.img[:, :, 2])
+            g = self.scale_img(self.img[:, :, 1])
+            b = self.scale_img(self.img[:, :, 0])
+            self.final_img = np.stack((r, g, b), axis = -1).astype(np.uint8)
 
-    def upscaling(self):
-        pass
-
-    def downscaling(self, matrix):
-        mod_matrix_v1 = np.zeros((matrix.shape[0], self.b))
-        mod_matrix = np.zeros((self.a, self.b))
-
-        for u in range(matrix.shape[0]):
-            x = 0
-            add = 0
-            pixels = 0
-            j = 0
-            for i in range(matrix.shape[1]):
-                if j >= 1:
-                    mod_matrix_v1[u, x] = add / pixels
-                    pixels = 0
-                    j -= 1
-                    add = 0
-                    x += 1
-                add += float(matrix[u, i])
-                j += self.scale
-                pixels += 1
-        for i in range(self.b):
-            y = 0
-            add = 0
-            pixels = 0
-            j = 0
-            for u in range(matrix.shape[0]):
-                if j >= 1:
-                    mod_matrix[y, i] = add / pixels
-                    pixels = 0
-                    j -= 1
-                    add = 0
-                    y += 1
-                add += mod_matrix_v1[u, i]
-                j += self.scale
-                pixels += 1
+    def scale_img(self, matrix):
+        a = int(matrix.shape[0] * self.scale)
+        b = int(matrix.shape[1] * self.scale)
+        mod_matrix_v1 = np.zeros((matrix.shape[0], b))
+        mod_matrix = np.zeros((a, b))
+        for y in range(mod_matrix_v1.shape[0]):
+            for x in range(mod_matrix_v1.shape[1]):
+                prev_x = x / self.scale
+                x_one = matrix[y, int(prev_x)] * (int(prev_x) + 1 - prev_x)
+                x_two = matrix[y, min(int(prev_x) + 1, matrix.shape[1] - 1)] * (prev_x - int(prev_x))
+                mod_matrix_v1[y, x] = x_one + x_two
+        for x in range(mod_matrix.shape[1]):
+            for y in range(mod_matrix.shape[0]):
+                prev_y = y / self.scale
+                y_one = mod_matrix_v1[int(prev_y), x] * (int(prev_y) + 1 - prev_y)
+                y_two = mod_matrix_v1[min(int(prev_y) + 1, mod_matrix_v1.shape[0] - 1), x] * (prev_y - int(prev_y))
+                mod_matrix[y, x] = y_one + y_two
         return mod_matrix
 
-
-
-
-
     def show(self) -> None:
-        plt.imshow(self.final_img)
+        if self.mode:
+            plt.imshow(self.final_img, cmap = "gray")
+        else:
+            plt.imshow(self.final_img)
         plt.axis("off")
         plt.show()
-
-
-
-
 
 if __name__ == "__main__":
     my_path = filedialog.askopenfilename()
