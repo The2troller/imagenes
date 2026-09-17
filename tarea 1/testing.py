5137610978bc6a608fa5c78e2631937f281dd6d3
The2Troller <renatosau@gmail.com>
2026-09-17 14:19:05 -0300

    imagenes necesarias para informe pregunta 1

--- tarea 1/testing.py
+++ tarea 1/testing.py
@@ -5,25 +5,32 @@ from pregunta_2 import Constrast
 import time
 from pregunta_3 import Scaling
 
+
+
+######
+import matplotlib.pyplot as plt
+#######
+
+
+
 if __name__ == "__main__":
     select = input("pregunta numero: ")
     if select == "1":
         my_path = "test_imgs/P1_IMG_2402.tif"
         img = cv2.imread(my_path)
-
         gray = ((0,0), (120, 0), (240, 0))
         yes_yellow = ((0, 1), (30, 1), (60, 4), (90, 1), (120, 1), (240, 1))
         no_green = ((0, 1), (90, 1), (120, 0), (150, 0.3), (240, 1), (170, 0.6))
         no_blue = ((120, 4), (150, 2.8),
                    (0, 1), (240, 0), (270, 0), (220, 0), (240, 0), (250, 0))
-        only_red = ((30, 0), (120, 0), (240, 0), (330, 0), (350, 0), (355, 0),
-                    (0, 4),(10, 4), (15, 4))
+        only_red = ((30, 1), (120, 1), (240, 1), (330, 1), (350, 1), (355, 1),
+                    (0, 30),(10, 30), (15, 30))
         selections = [gray, yes_yellow, no_green, no_blue, only_red]
         print("1. gray")
         print("2. yes_yellow")
         print("3. no_green")
         print("4. yes_yellow / no_blue")
-        print("5. only_red_more")
+        print("5. red_more")
         pts = selections[int(input("prueba :")) - 1]
         print("0. HSI")
         print("1. Lch")
@@ -35,7 +42,7 @@ if __name__ == "__main__":
         colorsaturation.show()
 
 
-        
+    #plt.imsave("modified_imgs/p1cr7_no_green_hsi.png", colorsaturation.final_img)
     elif select == "2":
         my_path = "test_imgs/P2_IMG_2423.tif"
         print("0. Contraste global")
@@ -51,6 +58,7 @@ if __name__ == "__main__":
             contrast.full_image()
             print(f"Tiempo tardado: {round(time.time() - one, 1)} segundos")
         contrast.show()
+
     elif select == "3":
         my_path = "test_imgs/P3_IMG_2387_crop.tif"
         print("0. 0.5")
